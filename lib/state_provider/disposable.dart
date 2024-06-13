b25ba0431c79961bb2ff39aa9ea7e429a337cb4c
Sander Roest <s.roest@dalosy.com>
2024-06-13 09:52:25 +0200

    Implement dispose method

--- lib/state_provider/disposable.dart
+++ lib/state_provider/disposable.dart
@@ -1,3 +1,7 @@
+/// Implement dispose method
+///
+/// If a state class implements this, it gets called whenever the
+/// StateProvider is removed from the widget tree.
 abstract class Disposable {
   void dispose();
 }
