091558c1b1517d43aca761c70826bb7b5cc21fb6
Sander Roest <s.roest@dalosy.com>
2024-06-13 10:51:12 +0200

    Explain multiple states of the same type

--- lib/states/multi_counters.dart
+++ lib/states/multi_counters.dart
@@ -1,5 +1,12 @@
 import 'package:poc/states/counter_state.dart';
 
+/*
+When you use StateProvider.of<T>, the type T is used to find the instance of
+this type in the widget tree. It is not possible to look up multiple instances
+of exact the same type. The classes below make it possible to use a type more
+than once by introducing a different type for each instance.
+ */
+
 class MultiCounter1 extends CounterState {
   MultiCounter1({super.debugLabel});
 }
