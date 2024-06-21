0582113ddca0cd30d653d0f6d2bfa862cfe87f1c
Sander Roest <s.roest@dalosy.com>
2024-06-21 13:30:51 +0200

    Use nested to prevent deep nesting

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:poc/pages/s010_main_menu/s010_main_menu_controller.dart';
 import 'package:poc/pages/s010_main_menu/s010_main_menu_page.dart';
 import 'package:poc/pages/s020_shared_counter/shared_counter_state.dart';
+import 'package:poc/state_provider/nested.dart';
 import 'package:poc/state_provider/state_provider.dart';
 
 void main() {
@@ -17,19 +18,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StateProvider<SharedCounterState>(
-      (context) => SharedCounterState(),
-      child: StateProvider(
-        (context) => S010MainMenuController(getNavigator),
-        child: MaterialApp(
-          navigatorKey: navigatorKey,
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: const S010MainMenuPage(),
+    return Nested(
+      children: [
+        StateProvider((_) => SharedCounterState()),
+        StateProvider((_) => S010MainMenuController(getNavigator))
+      ],
+      child: MaterialApp(
+        navigatorKey: navigatorKey,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
+        home: const S010MainMenuPage(),
       ),
     );
   }
